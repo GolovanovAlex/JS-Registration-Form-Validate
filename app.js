d8edf402e8797bfc2891e603b81f18b5b084e5c1
GolovanovAlex <Golovanov.a.a@yandex.ru>
2022-12-31 09:40:44 +0300

    js

--- app.js
+++ app.js
@@ -4,4 +4,20 @@ const form = document.querySelector('form'),
   passField = form.querySelector('.create-password'),
   passInput = passField.querySelector('.password'),
   conPassField = form.querySelector('.confirm-password'),
-  conPassInput = passField.querySelector('.confirpassword');
+  conPassInput = conPassField.querySelector('.confirpassword');
+
+// ---- ---- Email Validation ---- ---- //
+function checkEmail() {
+  const emailPattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+  if (!emailInput.value.match(emailPattern)) {
+    return emailField.classList.add('invalid');
+  }
+  emailField.classList.remove('invalid');
+}
+
+// ---- ---- Calling Function on Form Sumbit ---- ---- //
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  checkEmail();
+  emailInput.addEventListener('keyup', checkEmail);
+});
