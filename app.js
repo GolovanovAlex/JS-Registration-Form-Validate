f74c329652ac2a599cb9ca68561bcad6fd8cb8f7
GolovanovAlex <Golovanov.a.a@yandex.ru>
2022-12-31 09:19:31 +0300

    js

---
+++ app.js
@@ -0,0 +1,7 @@
+const form = document.querySelector('form'),
+  emailField = form.querySelector('.email-field'),
+  emailInput = emailField.querySelector('.email'),
+  passField = form.querySelector('.create-password'),
+  passInput = passField.querySelector('.password'),
+  conPassField = form.querySelector('.confirm-password'),
+  conPassInput = passField.querySelector('.confirpassword');
