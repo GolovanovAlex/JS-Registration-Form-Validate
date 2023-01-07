09dbaa598b36ebf71c381e94afd79df4f11e4ada
GolovanovAlex <Golovanov.a.a@yandex.ru>
2023-01-07 07:05:51 +0300

    js

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const form = document.querySelector('form'),
   emailInput = emailField.querySelector('.email'),
   passField = form.querySelector('.create-password'),
   passInput = passField.querySelector('.password'),
-  conPassField = form.querySelector('.confirm-password'),
-  conPassInput = conPassField.querySelector('.confirpassword');
+  cPassField = form.querySelector('.confirm-password'),
+  cPassInput = cPassField.querySelector('.confirpassword');
 
 // ---- ---- Email Validation ---- ---- //
 function checkEmail() {
@@ -19,13 +19,40 @@ const eyeIcons = document.querySelectorAll('.password-hide');
 eyeIcons.forEach((eyeIcon) => {
   eyeIcon.addEventListener('click', () => {
     const pInput = eyeIcon.parentElement.querySelector('input');
-    eyeIcon.classList.replace('bx-hide', 'bx-show');
+    if (pInput.type === 'password') {
+      eyeIcon.classList.replace('bx-hide', 'bx-show');
+      return (pInput.type = 'text');
+    }
+    eyeIcon.classList.replace('bx-show', 'bx-hide');
+    return (pInput.type = 'password');
   });
 });
+
+// ---- ---- Password Validation ---- ---- //
+function createPass() {
+  const passPattern =
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+
+  if (!passInput.value.match(passPattern)) {
+    return passField.classList.add('invalid');
+  }
+  passField.classList.remove('invalid');
+}
+// ---- ---- Confirm Password Validation ---- ---- //
+function confirmPass() {
+  if (passInput.value !== cPassInput.value || cPassInput.value === '') {
+    return cPassField.classList.add('invalid');
+  }
+  cPassField.classList.remove('invalid');
+}
 
 // ---- ---- Calling Function on Form Sumbit ---- ---- //
 form.addEventListener('submit', (e) => {
   e.preventDefault();
   checkEmail();
+  createPass();
+  confirmPass();
   emailInput.addEventListener('keyup', checkEmail);
+  passInput.addEventListener('keyup', createPass);
+  cPassInput.addEventListener('keyup', confirmPass);
 });
